2ed3a5f879215d06afb4c666c7428caa7aa0af05
newbi462 <isthisemailfree@gmail.com>
2020-01-27 16:53:08 -0500

    POST | /api/login

--- routes/userModel.js
+++ routes/userModel.js
@@ -3,20 +3,27 @@ const db = require('./../data/db.js');
 module.exports = {
   findUserById,
   addUser,
+  findByProp,
 }
 
 function findUserById(id) {
-    return db("users")
-        .select("id", "user", "password")
-        .where({ id })
-        .first();
+  return db("users")
+    .select("id", "user", "password")// added pass so I could see hash for token
+    .where({ id })
+    .first();
 }
 
 function addUser(userObj) {
+  return db("users")
+    .insert(userObj, "id")
+    .then(ids => {
+      const [id] = ids;
+      return findUserById(id);
+    });
+}
+
+function findByProp(filterBy) {
     return db("users")
-        .insert(userObj, "id")
-        .then(ids => {
-            const [id] = ids;
-            return findUserById(id);
-        });
+        .select("id", "user", "password")
+        .where(filterBy);
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,34 +6,43 @@ const UserModel = require('./userModel.js');
 const router = express.Router();
 
 
-router.post('/dumy', (request, responce) => {
-  db("cars").insert(request.body)
-    .then(idOfAdded => {
-      db("cars").where({id:idOfAdded[0]})
-      .then(acc => { responce.json(acc); })
-      .catch( error => {
-        console.log(error);
-        responce.status(500).json( {error: "Where by ID Failed."} )
-      })
+router.post("/register", (request, responce) => {
+  let userObj = request.body;
+  const hash = bcryptjs.hashSync(request.body.password, 8);// 2^8 hash
+  userObj.password = hash;// based on what I called this in my table object
+
+  UserModel.addUser(userObj)
+    .then(saved => {
+      responce.status(201).json(saved);
     })
-    .catch( error => {
-      console.log(error);
-      responce.status(500).json( {error: "Post Failed."} )
-    })
+    .catch(error => {
+      responce.status(500).json(error);
+    });
 });
 
-router.post("/register", (request, responce) => {
-    let userObj = request.body;
-    const hash = bcryptjs.hashSync(request.body.password, 8);// 2^8 hash
-    userObj.password = hash;// based on what I called this in my table object
 
-    UserModel.addUser(userObj)
-        .then(saved => {
-            responce.status(201).json(saved);
-        })
-        .catch(error => {
-            responce.status(500).json(error);
-        });
+router.post("/login", (request, responce) => {
+  let { user, password } = request.body;
+
+  UserModel.findByProp({ user }).first()//first() assumes user is the first find
+    .then(userObj => {
+      if (userObj && bcryptjs.compareSync(password, userObj.password)) {
+        // if (user) {
+        // compare().then(match => {
+        //   if (match) {
+        //     // good password
+        //   } else {
+        //     // they don't match
+        //   }
+        // }).catch()
+        responce.status(200).json({ message: `Welcome ${userObj.user}!, you are Logged In` });
+      } else {
+        responce.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      responce.status(500).json(error);
+    });
 });
 
 module.exports = router;
